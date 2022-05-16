e19978cb8e86ee5baa18fd211b2eba85983c7460
hoangviet148 <nnvvhh148>
2022-05-16 02:40:46 +0000

    update

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ### 1. Init ssh connection
 ```
-bash ssh-copy.sh
+bash ssh-copy.sh $USER
 ```
 
 ### 2. Install cluster
--- ssh-copy.sh
+++ ssh-copy.sh
@@ -5,6 +5,6 @@
 for x in 128 129 130
 do
     echo 192.168.75.$x
-    sshpass -f ./password.txt ssh-copy-id -i /root/.ssh/id_rsa.pub -o StrictHostKeyChecking=no ansible@192.168.75.$x
+    sshpass -f ./password.txt ssh-copy-id -i /root/.ssh/id_rsa.pub -o StrictHostKeyChecking=no $1@192.168.75.$x
 done
 
